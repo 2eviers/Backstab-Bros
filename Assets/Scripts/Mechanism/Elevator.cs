60e7f9735718fd96e444b0582070b02314087f10
Loïc Defrance <loic.defrance@gmail.com>
2015-01-24 04:14:26 +0100

    Updated Jaw and Elevator to match the new Mechanism

---
+++ Assets/Scripts/Mechanism/Elevator.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elevator : Mechanism {
+
+    public GameObject Platform;
+    public GameObject Axis;
+    public float Speed;
+
+    private int _upward;
+    private float _originalHeight;
+
+    void Start()
+    {
+        _upward = 1;
+        _originalHeight = Platform.transform.position.y;
+    }
+
+    protected override void runMechanism()
+    {
+        float height = Axis.transform.localScale.y;
+        float dist = Platform.transform.position.y - Axis.transform.position.y;
+
+        // si on est en haut, _upward = false
+        if (Platform.transform.position.y - Axis.transform.position.y >= height)
+            _upward = -1;
+        else if (Platform.transform.position.y - Axis.transform.position.y <= -height)
+            _upward = 1;
+
+        Vector3 dir = new Vector3(0, 1, 0);
+
+        Platform.transform.Translate(dir * Speed * _upward * Time.deltaTime);
+    }
+
+    protected override void backToDefaultPosition()
+    {
+        Vector3 dir = new Vector3(0, 1, 0);
+        if (Platform.transform.position.y > _originalHeight)
+            _upward = -1;
+        else if (Platform.transform.position.y < _originalHeight)
+            _upward = 1;
+        else
+            return;
+        Platform.transform.Translate(dir * Speed * _upward * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Mechanism/Jaw.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jaw : Mechanism {
+    // Jaw trap : munches the player if they try to go through
+
+    public GameObject UpperJaw;
+    public GameObject LowerJaw;
+    public float Speed;
+
+    private bool _opening = false;
+    private float _jawDistance;
+    private Vector3 _direction;
+
+	void Start () {
+        _direction = new Vector3(0, 0, 0);
+        _direction = UpperJaw.transform.position - LowerJaw.transform.position;
+        _jawDistance = _direction.y;
+	}
+
+    protected override void runMechanism()
+    {
+        _direction = new Vector3(0, 1, 0);
+        if (UpperJaw.transform.position.y - transform.position.y > _jawDistance / 4)
+        {
+            UpperJaw.transform.Translate(- _direction * Speed * Time.deltaTime);
+            LowerJaw.transform.Translate(_direction * Speed * Time.deltaTime);
+        }
+    }
+
+    protected override void backToDefaultPosition()
+    {
+        if (UpperJaw.transform.position.y - transform.position.y < _jawDistance / 2)
+        {
+            UpperJaw.transform.Translate(_direction * Speed * Time.deltaTime);
+            LowerJaw.transform.Translate(-_direction * Speed * Time.deltaTime);
+        }
+    }
+}
